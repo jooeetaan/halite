240b2349c166ad7a377e00dd805e0f8253977c36
Joseph Tan <jt.156linux@gmail.com>
2019-01-08 21:12:05 -0500

    new Bot

--- MyBot.java
+++ MyBot.java
@@ -1,74 +1,67 @@
-import hlt.*; //java helper code for interacting witht he API
-import java.util.ArrayList; //array list for commands
+//baseline bot to beat
+//PITA to beat, refuses to move
+import hlt.*;
+import java.util.ArrayList;
 
-//Do not use println...
 public class MyBot
 {
-  public static void main(String [] args)
+  public static void main (String [] args)
   {
-    Game game = new Game(); //The game variable has map data. Do any processing here.
-    //10 seconds to do any processing
-    game.ready("BotNameHere"); //Once this is called, there is a 2 second timer started.
+    Game game = new Game();
 
-    Log.log("Player id is " + game.myId);
+    game.ready("NotAI");
 
-    //Player objects have id, shipyard, and halite amount.
     Player me = game.me;
     GameMap gameMap = game.gameMap;
-    /* METHODS - substitute name of player object
-    player.getship(ship_id);
-    player.getships(); -> returns all ship IDS;
-    player.has_ship(ship_id) -> does it exist
-    player.get_dropoff(dropoff_id) -> returns dropoff
-    player.get_dropoffs();
-    */
-
-    //game.players -> dictionary
-    //iterate by using
-      //for player in game.players
 
 
-    int counter = 0;
-    for(;;) //infinite game loop
-    //sends the game state to players and processes commands returns from players
+    for(int i = 0; i > -1; i++)
     {
-      game.updateFrame(); //gets new Frame
+      game.updateFrame();
       ArrayList<Command> commandQueue = new ArrayList<>();
 
-      //if (me.halite > Constants.SHIP_COST && !gameMap.at(me.shipyard).isOccupied())
-      //{
-
-      //}
-      if (counter == 0)
+      EntityId[] ships  = new EntityId[4];
+      if (i == 5)
       {
-        commandQueue.add(me.shipyard.spawn());
-        counter++;
-        game.endTurn(commandQueue);
-        continue;
+        int counter = 0;
+        for (Ship ship : me.ships.values())
+        {
+          ships[counter] = ship.id;
+          counter++;
+        }
       }
-      for(Ship ship : me.ships.values())
+      if(i < 5)
       {
-
-        if (counter == 1)
-        {
-          commandQueue.add(ship.move(Direction.NORTH));
-        }
-        else if (ship.isFull() == true)
-        {
-          commandQueue.add(ship.move(gameMap.naiveNavigate(ship, me.shipyard.position)));
-        }
-        else if(gameMap.at(ship.position).halite != 0 && gameMap.at(ship.position) != gameMap.at(me.shipyard))
-        {
-          commandQueue.add(ship.stayStill());
-        }
-        else if(ship.isFull() == false)
-        {
-          commandQueue.add(ship.move(Direction.NORTH));
-        }
-
+          if (i < 4) commandQueue.add(me.shipyard.spawn());
+          for (Ship ship : me.ships.values())
+          {
+            if (gameMap.at(ship.position) == gameMap.at(me.shipyard))
+            {
+              if (i == 1) commandQueue.add(ship.move(Direction.NORTH));
+              if (i == 2) commandQueue.add(ship.move(Direction.EAST));
+              if (i == 3) commandQueue.add(ship.move(Direction.SOUTH));
+              if (i == 4) commandQueue.add(ship.move(Direction.WEST));
+            }
+          }
       }
-      counter++;
-      //end turns with the command that need to be sent.
+      else
+      {
+        for(int j = 0; j < 4; j++)
+        {
+          for(Ship ship : me.ships.values())
+          {
+            if (ship.id.equals(ships[j]))
+            {
+              if (j == 1) commandQueue.add(ship.move(Direction.NORTH));
+              if (j == 2) commandQueue.add(ship.move(Direction.EAST));
+              if (j == 3) commandQueue.add(ship.move(Direction.SOUTH));
+              if (j == 4) commandQueue.add(ship.move(Direction.WEST));
+            }
+          }
+
+        }
+      }
+
       game.endTurn(commandQueue);
     }
   }
